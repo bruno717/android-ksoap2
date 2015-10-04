8c94b8b74b4ee006238a46aa0ee69944210343e2
bruno717 <brunooliveira_717@hotmail.com>
2015-10-04 20:08:16 -0300

    web service soap

---
+++ WsSoapExemplo/src/java/model/Aluno.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Aluno {
+
+    public Aluno() {
+        super();
+    }
+    
+    public Aluno(Integer id, String nome, String curso) {
+        this.id = id;
+        this.nome = nome;
+        this.curso = curso;
+    }
+
+    private Integer id;
+    private String nome;
+    private String curso;   
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the curso
+     */
+    public String getCurso() {
+        return curso;
+    }
+
+    /**
+     * @param curso the curso to set
+     */
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
