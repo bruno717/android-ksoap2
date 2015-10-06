23fff88904b005a0e9c4aeee7525de2fa1c542be
bruno717 <bruno.oliveira@redspark.io>
2015-10-06 17:58:52 -0300

    alteracao do link para localhost

--- ExemploKsoap2/app/src/main/java/com/example/brunooliveira/exemploksoap2/config/AccessConfig.java
+++ ExemploKsoap2/app/src/main/java/com/example/brunooliveira/exemploksoap2/config/AccessConfig.java
@@ -6,6 +6,6 @@ package com.example.brunooliveira.exemploksoap2.config;
 public class AccessConfig {
 
     public static final String NAMESPACE = "http://ws/";
-    public static final String URL = "http://192.168.1.4:8084/WsSoapExemplo/WebServiceSoap?WSDL";
+    public static final String URL = "http://127.0.0.1:8084/WsSoapExemplo/WebServiceSoap?WSDL";
 
 }
